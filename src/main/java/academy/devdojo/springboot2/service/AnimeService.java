83b1efb55763ac53692f7ee01c58718096c97754
Victor Godoy <victor.hgodoy@icloud.com>
2025-08-16 19:08:00 -0300

    start devdojo course springboot2

---
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -0,0 +1,15 @@
+package academy.devdojo.springboot2.service;
+
+import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AnimeService {
+//    private final AnimeRepository animeRepository;
+
+    public List<Anime> listAll(){
+        return List.of(new Anime(1L,"Jujutsu Kaisen"), new Anime(2L,"Dragon Ball Z"));
+    }
+}
